29bccd0942912792fb887a3e5e7635f0715e431d
donottouch <donottouch@KIRAN>
2019-12-12 21:51:52 -0700

    chnages

--- ThirdJavaPjt/src/FINDINGGRADES.java
+++ ThirdJavaPjt/src/FINDINGGRADES.java
@@ -1,45 +1,26 @@
-//import java.util.Scanner;
+
 public class FINDINGGRADES {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//		Scanner sc = new Scanner(System.in);
-//		System.out.println("Enter the Average Score:");
-//		int num = sc.nextInt();
-//	if (num>=90)	{
-//		System.out.println(num+ "%: grade A");
-//	}
-//		else if (num>=70 && num<90) {
-//			System.out.println(num+ "%: grade B");
-//		
-//		
-//		}
-//		else {
-//			System.out.println( num+ "%: gradec");
-//	}
-//	
-//	
-
-		studentgrades(23);
-		studentgrades(45);
-		studentgrades(90);
-
-//		-if the average score >=90% =>grade=A
-//				-if the average score >= 70% and <90% => grade=B
-//				-if the average score>=50% and <70% =>grade=C
-//				-if the average score<50% =>grade=F
-
+		int num=67;
+	if (num>=90)	{
+		System.out.println(num+ "%: grade A");
 	}
-
-	private static void studentgrades(int i) {
-		// TODO Auto-generated method stub
-		if (i >= 90) {
-			System.out.println(i + "%: grade A");
-		} else if (i >= 70 && i < 90) {
-			System.out.println(i + "%: grade B");
-
-		} else {
-			System.out.println(i + "%: gradec");
+		else if (num>=70 && num<90) {
+			System.out.println(num+ "%: grade B");
+		
+		
 		}
+		else {
+			System.out.println( num+ "%: gradec");
 	}
-}
+	
+
+
+		
+	}
+
+	
+	}
+
