91e13c23a5d385f56fb8d10b6f08fee5f9aed4ca
donottouch <donottouch@KIRAN>
2019-12-14 07:22:39 -0700

    completed

--- ThirdJavaPjt/src/PALINDROME.java
+++ ThirdJavaPjt/src/PALINDROME.java
@@ -3,13 +3,19 @@ public class PALINDROME {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		palindromenumber(1001);
+		int a=33, sum=0;
+		int temp=a;
+		int r;
+	    while(a>0)	{
+	    	r=a%10;
+	    sum= (sum*10)+ r;
+		a=a/10;
+	    }
+		if(temp==sum) {
+		System.out.println(sum+  "the given number is palindrome ");
+		}
+		
+		else {
+			System.out.println(sum + "not a palindrome");
+		}}}
 
-	}
-
-	private static void palindromenumber(int i) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
